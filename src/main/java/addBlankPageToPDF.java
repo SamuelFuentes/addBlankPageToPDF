baa3b165ef564a7b8d1f3e09807be86fe4093194
SamuelFuentes <samuel.fuentes@bosonit.com>
2023-01-31 08:37:11 +0100

    Add: addBlankPageToPDF

---
+++ src/main/java/addBlankPageToPDF.java
@@ -0,0 +1,60 @@
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.PDPage;
+
+import java.io.File;
+import java.io.IOException;
+
+public class addBlankPageToPDF {
+    //Lee la ruta del directorio desde la variable de entorno previamente definida y agrega una páginaen blanco a cada
+    //archivo PDF
+    public static void main(String[] args){
+        String directoryPath = System.getenv("VIAFIRMA_PATH");
+
+        verifyPath(directoryPath);
+
+    }
+
+    //Verifica si la ruta del directorio es valida y si contiene arvhicos PDF, entonces agrega una página en blanco a
+    //cada archivo PDF
+    private static void verifyPath(String directoryPath){
+        File directory = new File(directoryPath);
+
+        if (!directory.exists() || !directory.isDirectory()){
+            System.out.println("La ruta del directorio es inválida");
+            return;
+        }
+
+        File[] files = directory.listFiles();
+        if (files == null){
+            System.out.println("No se encontraron archivos en el directorio");
+            return;
+        }
+
+        for (File file : files){
+            if (isPDFFile(file)){
+                addBlankPage(file);
+            }
+        }
+
+    }
+
+    //Abre el archivo PDF y agrega una página en blanco al final del archivo
+    private static void addBlankPage(File file){
+        try {
+            PDDocument document = PDDocument.load(file);
+            PDPage blankPage = new PDPage();
+            document.addPage(blankPage);
+            document.save(file);
+            document.close();
+        } catch (IOException e) {
+            System.out.println("Error al agregar una página en blanco al archivo " + file.getName());
+        }
+
+    }
+
+    //Verifica si el archivo es un PDF
+    private static boolean isPDFFile(File file) {
+        return file.isFile() && file.getName().endsWith(".pdf");
+
+    }
+}
